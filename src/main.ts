4e47ae301105272d231e29e69961f31afd13cc2f
Satish9047 <satishprajapati930@gmail.com>
2023-12-24 11:17:13 +0545

    improved frontend structure

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-//reset css
+export const addTask = document.getElementById("task-input") as HTMLInputElement;
+const taskArray:string[] = [];
+addTask.addEventListener("keydown", (ev)=>{
+    if(ev.key === "Enter"){
+        const taskName = addTask.value.trim();
+        if(taskName !== ""){
+            taskArray.push(taskName);
+            console.log(taskArray);
+        }
+    }
+});
+
