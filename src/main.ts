9a95babf41871e09deba9b0ffb5ea4b5cc9ccb30
Satish9047 <satishprajapati930@gmail.com>
2023-12-25 15:53:09 +0545

    search functionality completed

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import 'normalize.css';
+//import 'normalize.css';
 
 //importing classes
 import {Task} from "./class/Task.ts";
@@ -8,6 +8,7 @@ import {TaskList} from "./class/TaskList.ts";
 const addTask = document.getElementById("task-input") as HTMLInputElement;
 const searchInput = document.getElementById("search-task") as HTMLInputElement;
 const taskListElement = document.getElementById("task-incompleted") as HTMLInputElement;
+const completedTaskListElement = document.getElementById("task-completed") as HTMLInputElement;
 
 //creating the instance of TaskList Class
 const taskList = new TaskList();
@@ -44,18 +45,23 @@ function renderTaskList(tasks: TaskList){
     if(!taskListElement) throw new Error("Can't find DOM Element");
 
     taskListElement.innerHTML = "";
+    completedTaskListElement.innerHTML = "";
     tasks.list.forEach((task)=>{
         //creating the li html element
         const element = document.createElement("li");
+        const completedElement = document.createElement("li");
 
         //creating the checkbox input
         const checkBox = document.createElement("input");
         checkBox.type = "checkbox"
+        checkBox.checked = task.isCompleted;
+        console.log(task.isCompleted);
 
         //adding event listiner to the checkbox
         checkBox.addEventListener("change", ()=>{
             toggleCompletedTask(task.id);
             renderTaskList(taskList);
+            //console.log(task.isCompleted);
         })
 
         // Creating a label for the checkbox
@@ -63,12 +69,18 @@ function renderTaskList(tasks: TaskList){
         label.appendChild(checkBox);
         label.appendChild(document.createTextNode(task.taskName));
 
-        element.appendChild(label);
-        taskListElement.appendChild(element);
+        if(!task.isCompleted){
+            element.appendChild(label);
+            taskListElement.appendChild(element);
+        }
+        if(task.isCompleted){
+            completedElement.appendChild(label);
+            completedTaskListElement.appendChild(completedElement);
+        }
     })
 }
 
-//adding event listener to the addTask input
+//adding event listener to addTask input
 addTask.addEventListener("keydown", (ev)=>{
     if(ev.key === "Enter"){
         const newTaskName = addTask.value.trim();
@@ -79,3 +91,16 @@ addTask.addEventListener("keydown", (ev)=>{
         }
     }
 });
+
+//adding event listener to search task input
+searchInput?.addEventListener("input", (ev)=>{
+    const searchParam = (ev.target as HTMLInputElement)?.value;
+    render(searchParam);
+})
+
+//creating the render function
+function render(searchParam: string = ""){
+    const filterTaskList = searchTaskItem(taskList, searchParam);
+    renderTaskList(filterTaskList);
+}
+
